87ebdfa1ce187a10b3eb37a85286628df373b934
andercastro <andersteven.castro@gmail.com>
2025-12-03 16:06:00 -0600

    login completo

---
+++ PruebaBAC.Web/Controllers/AccesoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PruebaBAC.Web.Models;
+using System.Text;
+
+namespace PruebaBAC.Web.Controllers
+{
+    public class AccesoController : Controller
+    {
+        private readonly string _rutaApi = "http://localhost:5058/api/Usuarios/login";
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel modelo)
+        {
+            if (!ModelState.IsValid) return View(modelo);
+
+            using (var cliente = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(modelo);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var respuesta = await cliente.PostAsync(_rutaApi, content);
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    var respuestaJson = await respuesta.Content.ReadAsStringAsync();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Error = "Usuario o contraseña incorrectos";
+                    return View(modelo);
+                }
+            }
+        }
+    }
+}
--- PruebaBAC.Web/Models/LoginViewModel.cs
+++ PruebaBAC.Web/Models/LoginViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace PruebaBAC.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaBAC.Web.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "El usuario es requerido")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
