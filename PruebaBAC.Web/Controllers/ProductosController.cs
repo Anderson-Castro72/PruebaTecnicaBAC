d7d5ea52ba93cfa85de81c60c90d17fd57728080
andercastro <andersteven.castro@gmail.com>
2025-12-03 21:55:57 -0600

    select Prodcutos

---
+++ PruebaBAC.Web/Controllers/ProductosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PruebaBAC.Web.Models;
+
+namespace PruebaBAC.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        private readonly string _baseApi = "http://localhost:5058/api/Productos";
+
+        public ProductosController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ProductoViewModel> lista = new List<ProductoViewModel>();
+
+            var respuesta = await _httpClient.GetAsync(_baseApi);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var json_respuesta = await respuesta.Content.ReadAsStringAsync();
+                lista = JsonConvert.DeserializeObject<List<ProductoViewModel>>(json_respuesta);
+            }
+
+            return View(lista);
+        }
+    }
+}
---
+++ PruebaBAC.Web/Models/ProductoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaBAC.Web.Models
+{
+    public class ProductoViewModel
+    {
+        public int IdPro { get; set; }
+        public string Codigo { get; set; }
+        public string Producto1 { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
